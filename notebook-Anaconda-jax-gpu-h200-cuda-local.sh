728896f4cb1c23d0566de180bc06cafd624e46c6
Claire Xiong <65437096+clairexxiong@users.noreply.github.com>
2025-08-11 16:43:59 -0400

    Add files via upload

---
+++ notebook-Anaconda-jax-gpu-h200-cuda-local.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH --job-name=jaxh200
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=16G
+#SBATCH -p mit_normal_gpu
+#SBATCH --time=01:00:00
+#SBATCH --gres=gpu:h200:1
+#SBATCH -o notebook_%A.out
+
+# Initialize environment modules
+source /etc/profile.d/modules.sh
+module purge
+module use /orcd/software/core/001/modulefiles
+module load cuda/12.4.0
+
+source "$HOME/anaconda3/bin/activate" jax_cuda_local
+export PYTHONNOUSERSITE=1
+
+export CUDA_HOME="$(dirname "$(dirname "$(readlink -f "$(which nvcc)")")")"
+export LD_LIBRARY_PATH="$CUDNN_HOME/lib:$CUDA_HOME/lib64:$CUDA_HOME/extras/CUPTI/lib64:${LD_LIBRARY_PATH:-}"
+export XLA_FLAGS="--xla_gpu_cuda_data_dir=$CUDA_HOME ${XLA_FLAGS:-}"
+
+export XLA_PYTHON_CLIENT_PREALLOCATE=false
+export XLA_PYTHON_CLIENT_ALLOCATOR=platform
+
+# Set the IP address and port
+IP=`hostname -i`
+PORT=`shuf -i 2000-65000 -n 1`
+
+# Run Jupyter notebook
+$HOME/anaconda3/bin/jupyter notebook --ip=$IP --port=$PORT --no-browser
