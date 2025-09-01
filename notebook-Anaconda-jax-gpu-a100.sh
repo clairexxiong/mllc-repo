6e0497cfbed9580a0e2a31155ce52fe8e0ef6d75
Claire Xiong <65437096+clairexxiong@users.noreply.github.com>
2025-08-31 21:08:33 -0400

    Update notebook-Anaconda-jax-gpu-a100.sh

--- notebook-Anaconda-jax-gpu-a100.sh
+++ notebook-Anaconda-jax-gpu-a100.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=jax_sloangpu
+#SBATCH --job-name=jax_sloangpu2
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=16G
 #SBATCH -p sched_mit_sloan_gpu_r8
-#SBATCH --time=02:00:00
-#SBATCH --gres=gpu:1
+#SBATCH --time=01:00:00
+#SBATCH --gres=gpu:2
 #SBATCH -o notebook_%A.out
+
+set -euo pipefail
 
 # Initialize environment modules
 source /etc/profile.d/modules.sh
@@ -15,13 +17,16 @@ source "$HOME/anaconda3/bin/activate" jax_gpu124
 module purge
 module use /orcd/software/core/001/modulefiles
 module load cuda/12.4.0
-# module load cudnn/9.8.0.87-cuda12
 
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
 echo "LD_LIBRARY_PATH now: $LD_LIBRARY_PATH"
 
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
 export XLA_PYTHON_CLIENT_ALLOCATOR=platform
+export JAX_PLATFORMS=cuda
+export NCCL_DEBUG=WARN
+export NCCL_P2P_DISABLE=0
+export NCCL_SHM_DISABLE=0
 
 # Set the IP address and port
 IP=`hostname -i`
